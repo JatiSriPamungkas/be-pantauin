c857824f641aed0bb94651de6de98ae6ee2243e5
Jati Sri Pamungkas <jatispamungkas357@gmail.com>
2025-07-23 04:11:33 +0700

    fix: count workers

--- src/models/workers.js
+++ src/models/workers.js
@@ -1,7 +1,7 @@
 import { dbPool } from "../config/database.js";
 
 export const getWorkers = async () => {
-	const SQLQuery = `SELECT * FROM workers;`;
+	const SQLQuery = `SELECT COUNT(*) AS total_worker FROM workers;`;
 
 	return await dbPool.execute(SQLQuery);
 };
